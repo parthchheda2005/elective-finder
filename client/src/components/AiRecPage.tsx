951f65360c0d15f14aee1deb4e6c50b10e6d7416
Parth Chheda <parth.h.chheda@gmail.com>
2024-12-28 23:10:22 -0800

    added flex-col to under construction page

--- client/src/components/AiRecPage.tsx
+++ client/src/components/AiRecPage.tsx
@@ -4,7 +4,7 @@ export default function AiRecPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex justify-center items-center flex-col">
       <button
         onClick={() => navigate("/course-page")}
         className="p-2 bg-indigo-200 text-[#242424] rounded transition-all duration-300 hover:bg-blue-300 hover:scale-105 w-fit mt-2 mb-4"
