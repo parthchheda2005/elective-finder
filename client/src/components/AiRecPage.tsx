f20a96b8dcdb13411acc88226d652eda17810eb4
Parth Chheda <parth.h.chheda@gmail.com>
2024-12-28 23:08:41 -0800

    made an under construction page

--- client/src/components/AiRecPage.tsx
+++ client/src/components/AiRecPage.tsx
@@ -1,3 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export default function AiRecPage() {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <button
+        onClick={() => navigate("/course-page")}
+        className="p-2 bg-indigo-200 text-[#242424] rounded transition-all duration-300 hover:bg-blue-300 hover:scale-105 w-fit mt-2 mb-4"
+      >
+        go look at courses.
+      </button>
+      <h1 className="text-3xl">under construction.</h1>
+    </div>
+  );
 }
